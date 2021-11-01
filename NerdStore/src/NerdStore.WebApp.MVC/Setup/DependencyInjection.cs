df0f12d8dfad99f8e8aade3eda6a3133a900516a
caiobarretta <caioabarretta@gmail.com>
2021-11-01 02:54:13 -0300

    Modelagem de Domínios Ricos Mão na massa - Modelagem Tática Conectando a camada de apresentação

---
+++ NerdStore/src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data;
+using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection service)
+        {
+            // Domain Bus (Mediator)
+            service.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalogo
+            service.AddScoped<IProdutoRepository, ProdutoRepository>();
+            service.AddScoped<IProdutoAppService, ProdutoAppService>();
+            service.AddScoped<IEstoqueService, EstoqueService>();
+            service.AddScoped<CatalogoContext>();
+        }
+    }
+}
