afab804abba4c7093459a7842cc14fa95781224f
caiobarretta <caioabarretta@gmail.com>
2021-11-08 18:14:06 -0300

    Modelagem de Domínios Ricos Mão na Massa - CQRS Integração de BC's - Processamento do pe

--- NerdStore/src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,10 @@ namespace NerdStore.Vendas.Application.Events
     public class PedidoEventHandler :
         INotificationHandler<PedidoRascunhoIniciadoEvent>,
         INotificationHandler<PedidoAtualizadoEvent>,
-        INotificationHandler<PedidoItemAdicionadoEvent>
+        INotificationHandler<PedidoItemAdicionadoEvent>,
+        INotificationHandler<PedidoEstoqueRejeitadoEvent>
     {
+
         public Task Handle(PedidoRascunhoIniciadoEvent notification, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
@@ -26,6 +29,13 @@ namespace NerdStore.Vendas.Application.Events
         public Task Handle(PedidoItemAdicionadoEvent notification, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
+        }
+
+        public Task Handle(PedidoEstoqueRejeitadoEvent notification, CancellationToken cancellationToken)
+        {
+            // Cancelar o processamento do pedido - retornar erro para o cliente.
+            return Task.CompletedTask;
+
         }
     }
 }
